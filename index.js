862c4014366e14342bb0b90cb5cb7de4c54ef979
QuaziSamiha <1703155@student.ruet.ac.bd>
2022-03-11 12:09:55 +0600

    add admin section done

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 const express = require('express')
-const app = express()
 const cors = require('cors');
+const bodyParser = require('body-parser');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectID;
 const port = process.env.PORT || 3144
 
-
+const app = express()
 app.use(cors());
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.awb5t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-// console.log(uri);
-async function run() {
-    try {
-        await client.connect();
-        console.log('database connected successfully');
-    }
-    finally {
-        // await client.close()
-    }
-}
-
-run().catch(console.dir);
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello Real Estate Customers!!')
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dnk0j.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const adminCollection = client.db("SFR_Agency").collection("Admin");
+
+    app.post('/addAdmin', (req, res) => {
+        const newAdmin = req.body;
+        adminCollection.insertOne(newAdmin)
+            .then(result => {
+                res.send(result.insertedCount);
+            })
+    })
+
+
+    app.post('/isAdmin', (req, res) => {
+        const email = req.body.email;
+        adminCollection.find({ email: email })
+            .toArray((err, admins) => {
+                console.log(admins.length);
+                res.send(admins.length > 0)
+            })
+    })
+});
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
